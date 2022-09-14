2ce8a650472ff25a89cbe08e397f547e8c8e6493
Denis <you@example.com>
2022-09-14 20:50:16 +0400

    исправил ошибки

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 15;
+﻿int numberA = new Random().Next(5, 55);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(17, 67);
+Console.WriteLine(numberB);
 int summ = numberA + numberB;
 Console.WriteLine(summ);
--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username == "Денис");
+{
+    Console.WriteLine("Доброго дня Ваше величество " + username);
+}
+//else
+//{
+  //  Console.WriteLine("Гуляй холоп.. Я жду царя!");
+//}
