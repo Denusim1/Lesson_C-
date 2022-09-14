0f8e5acc4cdd1cab550fe59417db512e8918158f
Denis <you@example.com>
2022-09-14 22:10:30 +0400

    задачка по выбору максимального числа

---
+++ Example006/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 12;
+int b = 2;
+int c = 6;
+int d = 22;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+
+Console.Write("Максимально число = ");
+Console.WriteLine(max);
