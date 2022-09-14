27b310add45a6134aae60a0e53c9d3f3bd2d6c52
Denis <you@example.com>
2022-09-14 21:13:45 +0400

    про царя

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,11 +1,11 @@
-﻿Console.WriteLine("Введите имя: ");
+﻿Console.Write("Введите имя: ");
 string username = Console.ReadLine();
 
-if (username == "Денис");
+if (username == "Денис")
 {
     Console.WriteLine("Доброго дня Ваше величество " + username);
 }
-//else
-//{
-  //  Console.WriteLine("Гуляй холоп.. Я жду царя!");
-//}
+else
+{
+    Console.WriteLine("Гуляй холоп.. Я жду царя!");
+}
