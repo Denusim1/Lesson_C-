7ca028a5cf2e4811094e099d04af7d4af36ebbe7
Denis <you@example.com>
2022-09-19 02:59:12 +0400

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {14,42,3887,4,55,54,73,83};
+
+int n = array.Length;
+int find = 83;
+
+int index = -1;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    
+    index++;
+}
