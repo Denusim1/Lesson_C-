4cdd86725be883881045377bb5e1b753eb91fab6
Denis <you@example.com>
2022-09-21 01:37:39 +0400

    HomeWork005

---
+++ HomeWork005/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Write("Введите трехзначное число: ");
+int threeNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (threeNumber < 1000 )
+{
+    if ( threeNumber > 99)
+    {
+        Console.WriteLine("Второе число: " + CenterNumber(threeNumber));
+    }
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+}
+
+
+int CenterNumber(int N)
+{
+    int result = ((N / 10) % 10);
+    return result;
+}
+
+
+
+
