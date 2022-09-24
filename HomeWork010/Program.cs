ec1743baecd22dcf95bb9e50139ea91f7b20f5fe
Denis <you@example.com>
2022-09-24 13:19:19 +0400

    HomeWork010+screen

---
+++ HomeWork010/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+
+int number = Read("Введите число: ");
+
+
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{Math.Pow(i, 3)} ");
+}
+
+
+
+int Read(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
