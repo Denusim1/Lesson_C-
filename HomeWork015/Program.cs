5b70c8b8c73dc7548c90ae73a4471af29e37e5b9
Denis <you@example.com>
2022-10-05 14:15:05 +0400

    HomeWork015+screen

---
+++ HomeWork015/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+
+
+int size = 4;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int SumNumbers(int[] numbers)
+{
+    int summ = 0;
+    for (int i = 1; i < numbers.Length; i += 2)
+    {
+        summ += numbers[i];
+    }
+    return summ;
+}
+
+
+
+Console.Write("Полученный массив: ");
+PrintArray(numbers);
+int Summ = SumNumbers(numbers);
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях равна {Summ}.");
